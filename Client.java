46d4fcc197a88c99b574e5036f40c08a1e11c28d
abauer <ambauer@utexas.edu>
2016-11-28 22:37:35 -0600

    added create group window, listed todos, organized contact/contactlist

--- Client.java
+++ Client.java
@@ -3,11 +3,13 @@ package assignment7;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -27,18 +29,22 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
-import javax.swing.event.ChangeListener;
-
 
 public class Client extends Application {
 	// IO streams 
 	DataOutputStream toServer = null; 
 	DataInputStream fromServer = null;
+    Thread recieve;
+    ServerRecieve sa;
 	int userId;
     ArrayList<Contact> groups;
+    ContactList<Contact> groupsView;
     ArrayList<Contact> onlineFriends;
+    ContactList<Contact> onlineFriendsView;
     ArrayList<Contact> offlineFriends;
+    ContactList<Contact> offlineFriendsView;
     ArrayList<Contact> onlineStrangers;
+    ContactList<Contact> onlineStrangersView;
 
 
 	@Override // Override the start method in the Application class 
@@ -56,7 +62,7 @@ public class Client extends Application {
 		serverBox.setAlignment(Pos.CENTER);
 
 		Label serverLabel = new Label("Server Hostname: ");
-		TextField ipField = new TextField("localhost");
+		TextField ipField = new TextField("localhost"); //default
 		serverBox.getChildren().addAll(serverLabel,ipField);
 
 		loginBox.getChildren().add(serverBox);
@@ -66,7 +72,7 @@ public class Client extends Application {
 		usernameBox.setAlignment(Pos.CENTER);
 
 		Label usernameLabel = new Label("Username: ");
-		TextField usernameField = new TextField("demo");
+		TextField usernameField = new TextField("demo");    //TODO REMOVE
 		usernameBox.getChildren().addAll(usernameLabel,usernameField);
 
 		loginBox.getChildren().add(usernameBox);
@@ -77,7 +83,7 @@ public class Client extends Application {
 
 		Label passwordLabel = new Label("Password: ");
 		PasswordField passwordField = new PasswordField();
-		passwordField.setText("grantisawesome");
+		passwordField.setText("grantisawesome");    //TODO REMOVE
 		passwordBox.getChildren().addAll(passwordLabel,passwordField);
 
 		loginBox.getChildren().add(passwordBox);
@@ -101,7 +107,7 @@ public class Client extends Application {
 			loginStage.close();
 		});
 		Button loginButton = new Button("Login");
-		loginButton.setOnAction(e -> {
+		loginButton.setOnAction(e -> {  //TODO UNCOMMENT
 	//		boolean success = connectToServer(ipField.getText(),statusLabel);
 	//		if(!success)
 	//			return;
@@ -122,8 +128,65 @@ public class Client extends Application {
 		loginStage.show(); // Display the stage
 	}
 
+	private void openAddGroup(){
+        Stage groupStage = new Stage();
+        BorderPane groupPane = new BorderPane();
+        groupPane.setPadding(new Insets(10));
+        //Group Name - Top
+        HBox nameBox = new HBox();
+        nameBox.setPadding(new Insets(0,0,5,0));
+        nameBox.setAlignment(Pos.BOTTOM_CENTER);
+        Label title = new Label("Create a Group");
+        title.setAlignment(Pos.BOTTOM_CENTER);
+        nameBox.getChildren().add(title);
+        groupPane.setTop(nameBox);
+        //Friends - Left
+        VBox friends = new VBox();
+        friends.setPadding(new Insets(0,5,0,0));
+        friends.setSpacing(5);
+        groupPane.setLeft(friends);
+
+        Label groupLabel = new Label("Friends");
+        List<Contact> compiled = Stream.concat(onlineFriends.stream(),offlineFriends.stream()).map(c -> new Contact(c.toString())).collect(Collectors.toList());
+        ContactList<Contact> items = new ContactList<>(compiled);
+        friends.getChildren().addAll(groupLabel,items);
+
+        //mouse handler
+        // TODO update list
+
+        //In Group - Center
+        VBox included = new VBox();
+        included.setSpacing(5);
+        groupPane.setCenter(included);
+
+        Label includedLabel = new Label("Included");
+        ContactList<Contact> includes = new ContactList<>();
+        included.getChildren().addAll(includedLabel,includes);
+
+        //Finish - Bottom
+        HBox createBox = new HBox();
+        createBox.setPadding(new Insets(5,0,0,0));
+        createBox.setAlignment(Pos.BOTTOM_CENTER);
+        Button createGroup = new Button("Finish");
+        createGroup.setOnAction(event -> {
+            //TODO send Server action
+            groupStage.hide();
+        });
+        createGroup.setAlignment(Pos.BOTTOM_CENTER);
+        createBox.getChildren().add(createGroup);
+        groupPane.setBottom(createBox);
+
+        Scene scene = new Scene(groupPane);
+        groupStage.setHeight(500);
+        groupStage.setTitle("Create Group"); // Set the stage title
+        groupStage.setScene(scene); // Place the scene in the stage
+        groupStage.show(); // Display the stage
+    }
+
 	private void openChat(){
 
+        //TODO request lists from server instead of hardcode
+        //get online people / friends
         groups = new ArrayList<>();
         groups.add(new Contact("Senior Design",true)); groups.add(new Contact("HackDFW",true)); groups.add(new Contact("Frist Allo",true));
 
@@ -135,7 +198,7 @@ public class Client extends Application {
 
         onlineStrangers = new ArrayList<>();
         onlineStrangers.add(new Contact("BruceBanner",false)); onlineStrangers.add(new Contact("BilboBaggins",false));
-
+        //finish TODO
         Stage chatStage = new Stage();
 
         BorderPane chatPane = new BorderPane();
@@ -153,26 +216,27 @@ public class Client extends Application {
         groupLabelAlign.getChildren().add(groupLabel);
         groupLabelAlign.setAlignment(Pos.BOTTOM_CENTER);
         Button addGroupButton = new Button("Add Group");
+        addGroupButton.setOnAction(event -> openAddGroup());
         groupTitle.setLeft(groupLabelAlign);
         groupTitle.setRight(addGroupButton);
         friends.getChildren().add(groupTitle);
-        ContactList<Contact> groupsView = new ContactList<>(groups);
+        groupsView = new ContactList<>(groups);
         friends.getChildren().add(groupsView);
 
 
         Label onlineFriendsLabel = new Label("Online Friends");
         friends.getChildren().add(onlineFriendsLabel);
-        ContactList<Contact> onlineFriendsView = new ContactList<>(onlineFriends);
+        onlineFriendsView = new ContactList<>(onlineFriends);
         friends.getChildren().add(onlineFriendsView);
 
         Label offlineFriendsLabel = new Label("Offline Friends");
         friends.getChildren().add(offlineFriendsLabel);
-        ContactList<Contact> offlineFriendsView = new ContactList<>(offlineFriends);
+        offlineFriendsView = new ContactList<>(offlineFriends);
         friends.getChildren().add(offlineFriendsView);
 
         Label strangerLabel = new Label("Strangers");
         friends.getChildren().add(strangerLabel);
-        ContactList<Contact> onlineStrangersView = new ContactList<>(onlineStrangers);
+        onlineStrangersView = new ContactList<>(onlineStrangers);
         friends.getChildren().add(onlineStrangersView);
 
         //Chat - Right
@@ -185,6 +249,7 @@ public class Client extends Application {
 
         HBox sendMessageBox = new HBox();
         sendMessageBox.setPadding(new Insets(10,0,0,0));
+        sendMessageBox.setSpacing(5);
 
         TextField sendMessageField = new TextField();
         Button sendMessageButton = new Button("Send");
@@ -192,12 +257,13 @@ public class Client extends Application {
         HBox.setHgrow(sendMessageField, Priority.ALWAYS);
         chat.setBottom(sendMessageBox);
 
+        //TODO REMOVE
         chatPane.layoutBoundsProperty().addListener((observable, oldValue, newValue) -> {
             System.out.println("h: "+newValue.getHeight()+" w: "+newValue.getWidth());
         });
 
         // Create a scene and place it in the stage
-        Scene s = new Scene(chatPane,650,490);
+        Scene s = new Scene(chatPane,650,500);
         chatStage.setTitle("Chat Window"); // Set the stage title
         chatStage.setScene(s); // Place the scene in the stage
         chatStage.show(); // Display the stage
@@ -234,6 +300,11 @@ public class Client extends Application {
 
 			//wait for response
 			userId = fromServer.readInt();
+
+            //setup new Thread to recieve from Server
+            sa = new ServerRecieve(fromServer);
+            recieve = new Thread(sa);
+            recieve.start();
 		} catch (IOException ex){
 			ex.printStackTrace();
 		}
@@ -252,31 +323,60 @@ class Contact extends BorderPane{
     Shape notify;
     Shape addFriend;
     Shape unfriend;
-    HBox notifications;
+    HBox left;
+    HBox right;
+
+    public Contact(String name){
+        super();
+        this.name = new Label(name);
+        HBox nameBox = new HBox();
+        nameBox.setAlignment(Pos.CENTER_LEFT);
+        nameBox.getChildren().add(this.name);
+        nameBox.setPadding(new Insets(0,0,0,5));
+        setCenter(nameBox);
+    }
 
     public Contact(String name, boolean friend){
-        this.name = new Label(name);
-        notifications = new HBox();
-        notifications.setSpacing(5);
-        Contact myself = this;
-        addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            System.out.println("i was clicked: i am "+myself.name.getText());
-            toggleUnread();
-        });
-        notify = new Circle(5);
-        notify.setFill(Color.ORANGERED);
-        addFriend = new Plus(5,5);
-        addFriend.setFill(Color.GREEN);
-        unfriend = new Minus(5,5);
-        unfriend.setFill(Color.RED);
+        this(name);
         this.friend = friend;
-        setLeft(this.name);
-        setRight(notifications);
+
+        left = new HBox();
+        left.setSpacing(5);
+        left.setAlignment(Pos.CENTER_RIGHT);
+        right = new HBox();
+        right.setSpacing(5);
+        right.setAlignment(Pos.CENTER_RIGHT);
+        notify = new Circle(8);
+        notify.setFill(Color.ORANGE);
+        addFriend = new Plus(16,16);
+        addFriend.setFill(Color.GREEN);
+        unfriend = new Minus(16,16);
+        unfriend.setFill(Color.RED);
+
+        setHandlers();
+
+        setLeft(left);
+        setRight(right);
         recompileHBox();
     }
 
-    public void toggleFriend(){
-        friend = !friend;
+    private void setHandlers(){
+        Contact myself = this;
+        right.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            //TODO UNFRIEND/ADDFRIEND
+            System.out.println("unfriend was clicked");
+            toggleUnread();
+            event.consume();
+        });
+        addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            //TODO OPEN CHAT
+            System.out.println("i was clicked: i am "+myself.name.getText());
+        });
+    }
+
+    public void setFriend(boolean friend){
+        this.friend = friend;
+        //TODO CHANGE ARRAY
         recompileHBox();
     }
 
@@ -286,23 +386,24 @@ class Contact extends BorderPane{
     }
 
     private void recompileHBox() {
-        notifications.getChildren().clear();
+        right.getChildren().clear();
+        left.getChildren().clear();
         addFriend();
         addUnread();
     }
 
     private void addUnread(){
         if(unread){
-            notifications.getChildren().add(notify);
+            left.getChildren().add(notify);
         }
     }
 
     private void addFriend(){
         if(friend){
-            notifications.getChildren().add(unfriend);
+            right.getChildren().add(unfriend);
         }
         else{
-            notifications.getChildren().add(addFriend);
+            right.getChildren().add(addFriend);
         }
     }
 
@@ -314,43 +415,80 @@ class Contact extends BorderPane{
     }
 }
 
+class ServerRecieve implements Runnable {
+
+    DataInputStream fromServer;
+
+    public ServerRecieve(DataInputStream fs){
+        fromServer = fs;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                String server_response = fromServer.readUTF();
+                //parse server response
+            }
+            catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
 class ContactList<E> extends ListView<E> {
 
-    public ContactList(ArrayList<E> items){
+    public ContactList(){
         super();
         ContactList<E> myself = this;
         myself.getSelectionModel().selectedIndexProperty().addListener(e -> Platform.runLater(() -> myself.getSelectionModel().select(-1)));
+    }
+
+    public ContactList(List<E> items){
+        this();
+        setItems(FXCollections.observableArrayList(items));
+    }
+
+    public void removeItem(E item){
+        ArrayList<E> items = new ArrayList<>(getItems());
+        items.remove(item);
+        setItems(FXCollections.observableArrayList(items));
+    }
+
+    public void addItem(E item){
+        ArrayList<E> items = new ArrayList<>(getItems());
+        items.add(item);
         setItems(FXCollections.observableArrayList(items));
     }
 }
 
 class Plus extends Polygon{
     public Plus(double width, double height) {
-        width-=2;
-        height-=2;
         getPoints().addAll(
-                0.0, height,
-                width/2, 0.0,
-                width, height
+                0.0, height/4,
+                width/4,height/4,
+                width/4,0.0,
+                3*width/4,0.0,
+                3*width/4,height/4,
+                width,height/4,
+                width,3*height/4,
+                3*width/4,3*height/4,
+                3*width/4,height,
+                width/4,height,
+                width/4,3*height/4,
+                0.0,3*height/4
         );
     }
 }
 
 class Minus extends Polygon{
     public Minus(double width, double height) {
-        width-=2;
-        height-=2;
         getPoints().addAll(
                 0.0, height/4,
-                width/4, height/4,
-                width/2, 0.0,
-                3*width/4, height/4,
-                width, height/4,
-                3*width/4, 2*height/3,
-                7*width/8, height,
-                width/2, 3*height/4,
-                width/8, height,
-                width/4, 2*height/3
+                width,height/4,
+                width,3*height/4,
+                0.0,3*height/4
         );
     }
 }
