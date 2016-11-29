3802d2586d1151d04546db05e9257d781b9aeecd
grantuy <grant.uy@utexas.edu>
2016-11-28 22:26:30 -0600

    Fix Parser; Implement DatabaseAccessor

--- Parser.java
+++ Parser.java
@@ -3,11 +3,11 @@ package assignment7;
 import java.util.stream.Stream;
 
 public class Parser {
-    public String escapeString(String str) {
+    public static String escapeString(String str) {
         return str.replaceAll("\\s", "~ "); // escape with tilde
     }
 
-    public String[] parseString(String str) {
+    public static String[] parseString(String str) {
         return Stream.of(str.split("(?<!~)\\s")) // negative lookbehind
                 .map(s -> s.replace("~ ", " ")) // strip escape characters
                 .toArray(String[]::new);
